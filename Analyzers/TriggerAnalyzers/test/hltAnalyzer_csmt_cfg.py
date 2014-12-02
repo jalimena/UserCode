67052ccea9dd611602f30df6703e8f4ac1dcfbdf
Juliette <juliette.alimena@cern.ch>
2014-12-02 17:35:51 +0100

    add cosmic seed + meantimer configuration

---
+++ Analyzers/TriggerAnalyzers/test/hltAnalyzer_csmt_cfg.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+process.load("Analyzers.TriggerAnalyzers.hltAnalyzer_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+if 'GlobalTag' in process.__dict__:
+    from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
+    process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = 'MCRUN2_72_V3A')
+    process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_CONDITIONS'
+    process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
+    for pset in process.GlobalTag.toGet.value():
+        pset.connect = pset.connect.value().replace('frontier://FrontierProd/', 'frontier://FrontierProd/')
+    process.GlobalTag.RefreshEachRun = cms.untracked.bool( False )
+    process.GlobalTag.ReconnectEachRun = cms.untracked.bool( False )
+
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    )
+)
+
+process.demo.hltFilterTag_DiMuonL3NoFilters33 = cms.untracked.InputTag("hltL3fDimuonL1f0L2NVf10L3NoFiltersNoVtxCosmicSeedFiltered33","","TEST")
+process.demo.hltFilterTag_DiMuonL3NoFilters23Displaced = cms.untracked.InputTag("hltL3fDimuonL1f0L2NVf10L3NoFiltersNoVtxCosmicSeedDisplacedFiltered23","","TEST")
+process.demo.hltFilterTag_L2MuNoBptx20 = cms.untracked.InputTag("hltL2fL1sMu6NotBptxORL1f0NoVertexCosmicSeedL2Filtered20Sta3","","TEST")
+process.demo.hltFilterTag_L2DoubleMu28 = cms.untracked.InputTag("hltL2DoubleMu28NoVertexCosmicSeedL2Filtered2ChaAngle2p5Mass10","","TEST")
+process.demo.hltFilterTag_Mu38NoVtx_Photon38 = cms.untracked.InputTag("hltMu38NoFiltersNoVtxCosmicSeedPhoton38CaloIdLHEFilter","","TEST")
+process.demo.hltFilterTag_Mu28NoVtxDisplaced_Photon28 = cms.untracked.InputTag("hltMu28NoFiltersNoVtxCosmicSeedDisplacedPhoton28CaloIdLHEFilter","","TEST")
+process.demo.BarrelOnly = cms.untracked.bool(False)
+
+process.TFileService = cms.Service( "TFileService",
+                                    fileName = cms.string( 'histosHltAnalyzer_csmt.root' )
+                                    )
+
+
+process.p = cms.Path(process.demo)
---
+++ Analyzers/TriggerAnalyzers/test/hltAnalyzer_wocsmt_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+process.load("Analyzers.TriggerAnalyzers.hltAnalyzer_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+if 'GlobalTag' in process.__dict__:
+    from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
+    process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = 'MCRUN2_72_V3A')
+    process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_CONDITIONS'
+    process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
+    for pset in process.GlobalTag.toGet.value():
+        pset.connect = pset.connect.value().replace('frontier://FrontierProd/', 'frontier://FrontierProd/')
+    process.GlobalTag.RefreshEachRun = cms.untracked.bool( False )
+    process.GlobalTag.ReconnectEachRun = cms.untracked.bool( False )
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    )
+)
+
+process.TFileService = cms.Service( "TFileService",
+                                    fileName = cms.string( 'histosHltAnalyzer_wocsmt.root' )
+                                    )
+
+
+process.p = cms.Path(process.demo)
