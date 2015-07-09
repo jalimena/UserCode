072cb607be6fb6698339d9490069ce847157a438
Juliette Alimena <juliette.alimena@cern.ch>
2015-07-09 16:03:48 -0400

    add p instead of pt gun

---
+++ PGunMC/MuonParticleGunReco/step3_RAW2DIGI_HLT_L1Reco_RECO_SingleMuP250.py
@@ -0,0 +1,87 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step3 --filein file:step2_DIGI_L1_DIGI2RAW_HLT.root --step RAW2DIGI,HLT:2014,L1Reco,RECO --conditions PRE_STA72_V6::All --pileup NoPileUp --datamix NODATAMIXER --datatier GEN-SIM-RECO --eventcontent RECOSIM -n -1 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT2')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('HLTrigger.Configuration.HLT_2014_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+                            secondaryFileNames = cms.untracked.vstring(),
+                            fileNames = cms.untracked.vstring('file:/home/alimena/Analysis/CMSSW_7_1_0/src/MuonParticleGun/step1_SingleMuP250.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('step3 nevts:-1'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('step3_RAW2DIGI_HLT_L1Reco_RECO_SingleMuP250.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RECO')
+    )
+)
+
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'PRE_STA72_V6', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.RECOSIMoutput_step])
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
+
--- utility_macros/ListHaddStage/run_mchamps.csh
+++ utility_macros/ListHaddStage/run_mchamps.csh
@@ -1,7 +1,7 @@
 #!/bin/tcsh
 source hosts.csh
 
-foreach x (mchamp100_723patch1 mchamp200_723patch1 mchamp300_723patch1 mchamp400_723patch1 mchamp500_723patch1 mchamp600_723patch1 mchamp700_723patch1 mchamp800_723patch1 mchamp900_723patch1 mchamp1000_723patch1)
-#foreach x (PGunSingleMuPt250_fixedMatching)
+#foreach x (mchamp100_723patch1 mchamp200_723patch1 mchamp300_723patch1 mchamp400_723patch1 mchamp500_723patch1 mchamp600_723patch1 mchamp700_723patch1 mchamp800_723patch1 mchamp900_723patch1 mchamp1000_723patch1)
+foreach x (PGunSingleMuP250)
     source $p"ListHaddStage.csh" $x > & "out_"$x &
 end
