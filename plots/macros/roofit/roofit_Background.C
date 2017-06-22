2bcf938a82e35b992cf93d427ae4b38e165c9d45
Juliette <juliette.alimena@cern.ch>
2017-06-22 16:23:26 +0200

    updates

--- plots/macros/roofit/roofit_Background.C
+++ plots/macros/roofit/roofit_Background.C
@@ -62,8 +62,8 @@ void roofit_Background()
   RooGaussian gauss2("gauss2","gauss2",x,mean2,sigma2) ;
 
   RooRealVar mean3("mean3","mean3",-40,-55,-25) ;
-  RooRealVar sigma3("sigma3","sigma3",7,5,10) ;
-  //RooRealVar sigma3("sigma3","sigma3",1,0.1,2) ;
+  //RooRealVar sigma3("sigma3","sigma3",7,5,10) ; //crystal ball
+  RooRealVar sigma3("sigma3","sigma3",1,0.1,2) ; //landau
   RooGaussian gauss3("gauss3","gauss3",x,mean3,sigma3) ;
 
   RooRealVar alpha("alpha","alpha",-6,-10,-1);
@@ -76,15 +76,8 @@ void roofit_Background()
 
   RooRealVar frac1("frac1","events in gauss1",0.5,0.3,0.8) ;
   RooRealVar frac2("frac2","events in gauss2",0.4,0.2,0.7) ; 
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1,crystalBall),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1,landau),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(crystalBall),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(landau),RooArgList(frac1));
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2,gauss3),RooArgList(frac1,frac2));
-  //RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2,landau),RooArgList(frac1,frac2));
-  RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2,crystalBall),RooArgList(frac1,frac2));
+  //RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2,crystalBall),RooArgList(frac1,frac2));
+  RooAddPdf sum("sum","sum",RooArgList(gauss1,gauss2,landau),RooArgList(frac1,frac2));
 
   //crystalBall.fitTo(dh);
   //crystalBall.plotOn(frame) ;
@@ -94,12 +87,89 @@ void roofit_Background()
   sum.plotOn(frame) ;
   sum.paramOn(frame);
 
+
   x.setRange("signal",-20.,100);
   RooAbsReal* integral = sum.createIntegral(x,NormSet(x),Range("signal"));
-  cout << "Integral > -20 ns = " << integral->getVal()*hh->Integral() << endl; //fraction of fit > -20 * total number of events in histogram
+  double integralValueNominal = integral->getVal()*hh->Integral();
+  cout << "Integral > -20 ns = " << integralValueNominal << endl; //fraction of fit > -20 * total number of events in histogram
 
   cout<<"chisq is: "<<frame->chiSquare(3)<<endl;
 
+
+
+  RooRealVar mean1Lo("mean1Lo","mean1Lo",mean1.getVal()-mean1.getError()) ;
+  RooRealVar sigma1Lo("sigma1Lo","sigma1Lo",sigma1.getVal()-sigma1.getError()) ;
+  RooGaussian gauss1Lo("gauss1Lo","gauss1Lo",x,mean1Lo,sigma1Lo) ;
+
+  RooRealVar mean2Lo("mean2Lo","mean2Lo",mean2.getVal()-mean2.getError()) ;
+  RooRealVar sigma2Lo("sigma2Lo","sigma2Lo",sigma2.getVal()-sigma2.getError()) ;
+  RooGaussian gauss2Lo("gauss2Lo","gauss2Lo",x,mean2Lo,sigma2Lo) ;
+
+  RooRealVar mean3Lo("mean3Lo","mean3Lo",mean3.getVal()-mean3.getError()) ;
+  RooRealVar sigma3Lo("sigma3Lo","sigma3Lo",sigma3.getVal()-sigma3.getError()) ;
+  RooGaussian gauss3Lo("gauss3Lo","gauss3Lo",x,mean3Lo,sigma3Lo) ;
+
+
+  RooRealVar alphaLo("alphaLo","alphaLo",alpha.getVal()-alpha.getError()) ;
+  RooRealVar nLo("nLo","nLo",n.getVal()-n.getError()) ;
+
+  RooCBShape crystalBallLo("crystalBallLo","crystalBallLo",x,mean3Lo,sigma3Lo,alphaLo,nLo);
+
+  RooLandau landauLo("landauLo","landauLo",x,mean3Lo,sigma3Lo);
+
+  RooRealVar frac1Lo("frac1Lo","events in gauss1",frac1.getVal()-frac1.getError()) ;
+  RooRealVar frac2Lo("frac2Lo","events in gauss2",frac2.getVal()-frac2.getError()) ;
+  //RooAddPdf sumLo("sumLo","sumLo",RooArgList(gauss1Lo,gauss2Lo,crystalBallLo),RooArgList(frac1Lo,frac2Lo));
+  RooAddPdf sumLo("sumLo","sumLo",RooArgList(gauss1Lo,gauss2Lo,landauLo),RooArgList(frac1Lo,frac2Lo));
+
+  sumLo.plotOn(frame,LineColor(kRed)) ;
+
+  RooAbsReal* integralLo = sumLo.createIntegral(x,NormSet(x),Range("signal"));
+  double integralValueLo = integralLo->getVal()*hh->Integral();
+  cout << "IntegralLo > -20 ns = " << integralValueLo << endl; //fraction of fit > -20 * total number of events in histogram
+
+
+  RooRealVar mean1Hi("mean1Hi","mean1Hi",mean1.getVal()+mean1.getError()) ;
+  RooRealVar sigma1Hi("sigma1Hi","sigma1Hi",sigma1.getVal()+sigma1.getError()) ;
+  RooGaussian gauss1Hi("gauss1Hi","gauss1Hi",x,mean1Hi,sigma1Hi) ;
+
+  RooRealVar mean2Hi("mean2Hi","mean2Hi",mean2.getVal()+mean2.getError()) ;
+  RooRealVar sigma2Hi("sigma2Hi","sigma2Hi",sigma2.getVal()+sigma2.getError()) ;
+  RooGaussian gauss2Hi("gauss2Hi","gauss2Hi",x,mean2Hi,sigma2Hi) ;
+
+  RooRealVar mean3Hi("mean3Hi","mean3Hi",mean3.getVal()+mean3.getError()) ;
+  RooRealVar sigma3Hi("sigma3Hi","sigma3Hi",sigma3.getVal()+sigma3.getError()) ;
+  RooGaussian gauss3Hi("gauss3Hi","gauss3Hi",x,mean3Hi,sigma3Hi) ;
+
+
+  RooRealVar alphaHi("alphaHi","alphaHi",alpha.getVal()+alpha.getError()) ;
+  RooRealVar nHi("nHi","nHi",n.getVal()+n.getError()) ;
+
+  RooCBShape crystalBallHi("crystalBallHi","crystalBallHi",x,mean3Hi,sigma3Hi,alphaHi,nHi);
+
+  RooLandau landauHi("landauHi","landauHi",x,mean3Hi,sigma3Hi);
+
+  RooRealVar frac1Hi("frac1Hi","events in gauss1",frac1.getVal()+frac1.getError()) ;
+  RooRealVar frac2Hi("frac2Hi","events in gauss2",frac2.getVal()+frac2.getError()) ;
+  //RooAddPdf sumHi("sumHi","sumHi",RooArgList(gauss1Hi,gauss2Hi,crystalBallHi),RooArgList(frac1Hi,frac2Hi));
+  RooAddPdf sumHi("sumHi","sumHi",RooArgList(gauss1Hi,gauss2Hi,landauHi),RooArgList(frac1Hi,frac2Hi));
+
+  sumHi.plotOn(frame,LineColor(kGreen)) ;
+
+  RooAbsReal* integralHi = sumHi.createIntegral(x,NormSet(x),Range("signal"));
+  double integralValueHi = integralHi->getVal()*hh->Integral();
+  cout << "IntegralHi > -20 ns = " << integralValueHi << endl; //fraction of fit > -20 * total number of events in histogram
+
+  double integralValueMinus, integralValuePlus;
+  if((integralValueNominal-integralValueLo)> 0.) integralValuePlus = integralValueNominal-integralValueLo;
+  else integralValueMinus = integralValueLo-integralValueNominal;
+
+  if((integralValueNominal-integralValueHi)> 0.) integralValuePlus = integralValueNominal-integralValueHi;
+  else integralValueMinus = integralValueHi-integralValueNominal;
+
+ 
+
+  cout << "Integral > -20 ns = " << integralValueNominal <<" + "<<integralValuePlus  <<" - "<<integralValueMinus<< endl;
   // P l o t   a n d   f i t   a   R o o D a t a H i s t   w i t h   i n t e r n a l   e r r o r s
   // ---------------------------------------------------------------------------------------------
 
@@ -121,6 +191,7 @@ void roofit_Background()
 
   TCanvas* c1 = new TCanvas("c1","c1",800,800) ;
   c1->SetLogy();
+  frame->GetYaxis()->SetRangeUser(.1,100);
   frame->Draw();
   //c1->SaveAs("backgroundExtrapolation_2016data.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg50.pdf");
@@ -131,64 +202,62 @@ void roofit_Background()
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg25.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg20.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg15.pdf");
-  c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg10.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg10.pdf");
 
   //c1->SaveAs("backgroundExtrapolation_2016data_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg50_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg40_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg30_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg20_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg10_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg45_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg40_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg35_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg30_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg25_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg20_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg15_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg5_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2016data_DeltaTrpcNeg10_GausLandau.pdf");
 
   //c1->SaveAs("backgroundExtrapolation_2015data.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg50.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg40.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg30.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg20.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg10.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg45.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg40.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg35.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg30.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg25.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg20.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg15.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg5.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg10.pdf");
 
   //c1->SaveAs("backgroundExtrapolation_2015data_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg50_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg40_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg30_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg20_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg10_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg45_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg40_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg35_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg30_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg25_GausLandau.pdf");
+  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg20_GausLandau.pdf");
   //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg15_GausLandau.pdf");
-  //c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg5_GausLandau.pdf");
+  c1->SaveAs("backgroundExtrapolation_2015data_DeltaTrpcNeg10_GausLandau.pdf");
 
   //c1->SaveAs("backgroundExtrapolation_cosmicData.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg50.pdf");
-  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg40.pdf");
-  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg30.pdf");
-  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg20.pdf");
-  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg10.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg45.pdf");
+  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg40.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg35.pdf");
+  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg30.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg25.pdf");
+  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg20.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg15.pdf");
-  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg5.pdf");
+  //c1->SaveAs("backgroundExtrapolation_cosmicData_DeltaTrpcNeg10.pdf");
 
   //c1->SaveAs("backgroundExtrapolation_cosmicMC80X.pdf");
   //c1->SaveAs("backgroundExtrapolation_cosmicMC76X.pdf");
+
+  frame->Print("v");
 }
 
 TH1D* makeTH1() 
 {
-  TFile* f1 = new TFile("/afs/cern.ch/work/j/jalimena/CMSSW_8_0_22/src/NoBPTX_2016BCDEFGH_23Sep2016_Ntuple.root");
-  //TFile* f1 = new TFile("/afs/cern.ch/work/j/jalimena/CMSSW_8_0_22/src/NoBPTX_16Dec2015_Ntuple.root");
+  //TFile* f1 = new TFile("/afs/cern.ch/work/j/jalimena/CMSSW_8_0_22/src/NoBPTX_2016BCDEFGH_23Sep2016_Ntuple.root");
+  TFile* f1 = new TFile("/afs/cern.ch/work/j/jalimena/CMSSW_8_0_22/src/NoBPTX_16Dec2015_Ntuple.root");
 
   //TH1D* hh = (TH1D*) f1->Get("DelayedMuonsUpperLowerSelection2Plotter/Track-secondaryTrack Plots/delta_dtTofTimeAtIpInOut");
   //TH1D* hh = (TH1D*) f1->Get("DelayedMuonsUpperLowerSelection2DeltaTrpcTimeNeg50Plotter/Track-secondaryTrack Plots/delta_dtTofTimeAtIpInOut");
