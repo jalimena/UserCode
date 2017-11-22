bb0070015e3fb0c2b10061dbeb5501bdcd834351
Juliette Alimena <juliette.alimena@cern.ch>
2017-11-22 01:33:09 -0600

    update input dataset name

--- HSCPPhaseIIMC/crab_step2_HSCPppstau_M_1599_TuneCUETP8M1_14TeV_pythia8.py
+++ HSCPPhaseIIMC/crab_step2_HSCPppstau_M_1599_TuneCUETP8M1_14TeV_pythia8.py
@@ -10,7 +10,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step2_DIGI_L1_DIGI2RAW_HLT_PU.py'
 config.JobType.maxMemoryMB = 4000
 
-config.Data.inputDataset = ''
+config.Data.inputDataset = '/HSCPppstau_M_1599_TuneCUETP8M1_14TeV_pythia8_912/jalimena-GenSim-97eba4a2a2d9ef6e868ffc3071ab1506/USER'
 config.Data.outputDatasetTag = 'DigiHlt_PU200'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
